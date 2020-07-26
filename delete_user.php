34f6c4c740303d1fc2bef1716c6cc4eabad10872
David Ward <admin@pineapplesoft.co.uk>
2020-07-26 08:15:29 +0100

    Initial delete user functionality

---
+++ delete_user.php
@@ -0,0 +1,78 @@
+<?php
+//Security for includes
+$includes = TRUE;
+
+//Include file to open database
+$includeOpendbOk = include("open_db.php");
+if (!$includeOpendbOk) {
+  echo "An important file is missing or cannot be accessed: open_db.php";
+  exit();
+}
+
+//Include check to ensure user is logged in
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+  echo 'You must log in to view this page';
+  exit();
+}
+?>
+
+//Check to ensure user is Superuser
+if ($_SESSION['usertype'] != "superuser") {
+  header( "refresh:3;url=index.php" );
+  echo 'You must be a superuser to view this page, You will now be redirected automatically, or, click <a href="index.php">here</a>.';
+  exit();
+}
+?>
+
+<?php
+include("header.php");
+?>
+<h2>User management</h2>
+
+<?php
+
+//If form is not submitted..
+
+if (!isset($_POST['submit'])) {
+  //The following will clean up the 'id' variable passed un the URI...
+  $userId = filter_input(INPUT_GET, 'userId', FILTER_SANITIZE_SPECIAL_CHARS);
+  $username = filter_input(INPUT_GET, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  if (!isset($userId)) {
+    echo "Invalid user ID, the user ID passed in the URI does not exist in the database";
+    exit();
+  }
+
+  ?>
+  <p class="red">Are you sure you wish to continue and delete the user: <?php echo $username;?></p>
+  <form action="delete_user.php" method="post">
+
+    <input type="hidden" id="userId" name="userId" value="<?php echo $userId;?>">
+
+    <input type="submit" name="submit" value="Continue">
+  </form>
+  <?php
+}
+
+//If form is submitted...
+
+if (isset($_POST['submit'])) {
+
+  $userId = $_POST['userId'];
+
+  $stmt = $db->prepare("DELETE FROM users WHERE id = :id");
+  $stmt->bindValue(':id', $userId);
+  $result = $stmt->execute();
+  ?>
+
+  <p>User ID '<?php echo $userId;?>' was successfully deleted</p>
+
+  <?php
+}
+
+?>
+<?php
+include("footer.php");
+?>
---
+++ users.php
@@ -0,0 +1,71 @@
+<?php
+//Security for includes
+$includes = TRUE;
+
+//Include file to open database
+$includeOpendbOk = include("open_db.php");
+if (!$includeOpendbOk) {
+  echo "An important file is missing or cannot be accessed: open_db.php";
+  exit();
+}
+
+//Include check to ensure user is logged in
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+  echo 'You must log in to view this page';
+  exit();
+}
+?>
+
+//Check to ensure user is Superuser
+if ($_SESSION['usertype'] != "superuser") {
+  header( "refresh:3;url=index.php" );
+  echo 'You must be a superuser to view this page, You will now be redirected automatically, or, click <a href="index.php">here</a>.';
+  exit();
+}
+?>
+
+<?php
+include("header.php");
+?>
+
+<h1>User management</h1>
+
+<div id="tableContainer">    
+  <table id="faultTable">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>User Type</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>     
+      <?php
+      $res = $db->query('SELECT * FROM users');
+
+      while ($row = $res->fetchArray()) {
+
+        //PARANTHESES REMAIN OPEN FOR USE IN HTML BELOW
+
+      ?>
+
+        <tr>
+          <td><?php echo $row['id'];?></td>
+          <td><?php echo $row['username'];?></td>
+          <td><?php echo $row['userType'];?></td>
+          <td><a href="delete_user.php?userId=<?php echo $row['id'];?>&username=<?php echo $row['username'];?>">Delete User</a></td>
+        </tr>
+
+      <?php
+      } //End of query!  
+      ?>
+  </tbody>
+  </table>
+</div>
+
+<?php
+include("footer.php");
+?>
